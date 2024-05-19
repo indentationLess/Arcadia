1e2f713cf8063d01409c1a8b2f4a780bcb06dd3c
euphrosyne <urfavdigitalperson@gmail.com>
2024-05-19 22:10:27 +0300

    Add score display in Pong game

--- src/pong/pong.cpp
+++ src/pong/pong.cpp
@@ -12,8 +12,10 @@ void runPongGame() {
   Bat bat(300, 500);
   AIBat aiBat(300, 100);
   Ball ball(400, 250);
- int player_score = 0;
- int ai_score = 0;
+  int player_score = 0;
+  int ai_score = 0;
+  sf::Font font;
+  font.loadFromFile("includes/ClearSans.ttf");
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -26,10 +28,23 @@ void runPongGame() {
     ball.update();
 
     window.clear();
+
     window.draw(bat.getBatObject());
     window.draw(aiBat.getAIBatObject());
     window.draw(ball.getBallObject());
+
+    sf::Text playerScoreText("Player: " + std::to_string(player_score), font,
+                             20);
+    playerScoreText.setPosition(20.f, 20.f);
+    window.draw(playerScoreText);
+
+    sf::Text aiScoreText("AI: " + std::to_string(ai_score), font, 20);
+    aiScoreText.setPosition(
+        window.getSize().x - aiScoreText.getGlobalBounds().width - 20.f, 20.f);
+    window.draw(aiScoreText);
+
     window.display();
+
     if (ball.getposition().left <= 0 ||
         ball.getposition().left + ball.getposition().width >= 800) {
       ball.reboundSides();
@@ -56,22 +71,14 @@ void runPongGame() {
               "includes/sfx/jixaw-metal-pipe-falling-sound.mp3");
       moveSound->play();
     }
-    if (ball.getcords().x > aiBat.getAIBatPosition().x  &&
+    if (ball.getcords().x > aiBat.getAIBatPosition().x &&
         aiBat.getAIBatPosition().x + aiBat.getAIBatFloatRect().width < 800) {
       aiBat.moveAIBatRight();
     }
-    if (ball.getcords().x < aiBat.getAIBatPosition().x  &&
+    if (ball.getcords().x < aiBat.getAIBatPosition().x &&
         aiBat.getAIBatPosition().x > 0) {
       aiBat.moveAIBatLeft();
     }
-sf::Font font;
-        sf::Text playerScoreText("Player: " + std::to_string(player_score), font, 20);
-        playerScoreText.setPosition(20.f, 20.f);
-        window.draw(playerScoreText);
-
-        sf::Text aiScoreText("AI: " + std::to_string(ai_score), font, 20);
-        aiScoreText.setPosition(window.getSize().x - aiScoreText.getGlobalBounds().width - 20.f, 20.f);
-        window.draw(aiScoreText);
   }
 }
 
